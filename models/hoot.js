94463207d106e5f4ea83967372d650a1e7432f35
Amogh <amogh.maheshwari@email.com>
2025-01-09 11:07:01 -0800

    finished defining the models (hoot, user, and comment schema)

---
+++ models/hoot.js
@@ -0,0 +1,42 @@
+// comment schema which is embedded inside hoot model
+const commentSchema = new mongoose.Schema(
+    {
+      text: {
+        type: String,
+        required: true
+      },
+      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User' }
+    },
+    { timestamps: true }
+  );
+
+
+// hoot model
+const hootSchema = new mongoose.Schema(
+    {
+    title: {
+        type: String,
+        required: true,
+    },
+    text: {
+        type: String,
+        required: true,
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['News', 'Sports', 'Games', 'Movies', 'Music', 'Television'],
+    },
+      author: { type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+      comments: [commentSchema]
+    },
+    { timestamps: true }
+  );
+    // registering the model
+    const Hoot = mongoose.model('Hoot', hootSchema);
+
+    // exporting the model
+    module.exports = Hoot;
+
+
+  
